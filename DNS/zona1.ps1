619a64def63aae0d54dd81e4a99b0c4da424bc54
ASIR210 <mfernandeza16@educantabria.es>
2021-10-20 17:23:23 +0200

    Update zona1.ps1

--- DNS/zona1.ps1
+++ DNS/zona1.ps1
@@ -4,5 +4,7 @@
 #####################################################
 Remove-DnsServerZone -name miercoles10.com -Force
 Add-DnsServerPrimaryZone  -Name miercoles10.com -ZoneFile miercoles15.com.dns
-
+Add-DnsServerResourceRecordA -Name ns1 -IPv4Address 172.20.140.110 -ZoneName miercoles10.com 
+Add-DnsServerResourceRecordCName -Name www -HostNameAlias ns1.miercoles10.com 
+Add-DnsServerResourceRecord -Name "@" -NameServer ns1.miercoles10.com -NS -ZoneName miercoles10.com
 
