e0da274dfe3c0d67764dda3b1c06ee57f0324aa0
ASIR210 <mfernandeza16@educantabria.es>
2021-10-20 17:11:44 +0200

    Create zona1.ps1

---
+++ DNS/zona1.ps1
@@ -0,0 +1,8 @@
+﻿#####################################################
+###########	Martin Fernandez Aizcorbe	#############
+###########	20/10/2021	#############################
+#####################################################
+Remove-DnsServerZone -name miercoles10.com -Force
+Add-DnsServerPrimaryZone  -Name miercoles10.com -ZoneFile miercoles15.com.dns
+
+
